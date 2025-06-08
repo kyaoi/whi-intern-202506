bf2cfbfc6fa2ca1bb75522d1ec7851579f3edbe5
kyaoi <aoistudy90@gmail.com>
2025-06-08 10:27:17 +0900

    feat: completion of frontend searchAPI

--- frontend/src/components/EmployeeListContainer.tsx
+++ frontend/src/components/EmployeeListContainer.tsx
@@ -1,61 +1,62 @@
 "use client";
+import type { FilterOptions } from "@/types/employee";
+import { isLeft } from "fp-ts/Either";
+import * as t from "io-ts";
 import { useEffect } from "react";
 import useSWR from "swr";
-import * as t from "io-ts";
-import { isLeft } from "fp-ts/Either";
+import { type Employee, EmployeeT } from "../models/Employee";
 import { EmployeeListItem } from "./EmployeeListItem";
-import { Employee, EmployeeT } from "../models/Employee";
-import { FilterOptions } from "@/types/employee";
 
 export type EmployeesContainerProps = {
-  filterText: string;
-  filterDetail?: FilterOptions;
+	filterName: string;
+	filterDetail?: FilterOptions;
 };
 
 const EmployeesT = t.array(EmployeeT);
 
 const employeesFetcher = async (url: string): Promise<Employee[]> => {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch employees at ${url}`);
-  }
-  const body = await response.json();
-  const decoded = EmployeesT.decode(body);
-  if (isLeft(decoded)) {
-    throw new Error(`Failed to decode employees ${JSON.stringify(body)}`);
-  }
-  return decoded.right;
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error(`Failed to fetch employees at ${url}`);
+	}
+	const body = await response.json();
+	const decoded = EmployeesT.decode(body);
+	if (isLeft(decoded)) {
+		throw new Error(`Failed to decode employees ${JSON.stringify(body)}`);
+	}
+	return decoded.right;
 };
 
-export function EmployeeListContainer({ filterText, filterDetail }: EmployeesContainerProps) {
-  const searchParams = new URLSearchParams();
-  searchParams.append("filterText", filterText);
-  for (const key in filterDetail) {
-    const values = filterDetail[key as keyof FilterOptions];
-    if (values != null) {
-      values.forEach((v) => {
-        searchParams.append(key, v);
-      });
-    }
-  }
+export function EmployeeListContainer({
+	filterName,
+	filterDetail,
+}: EmployeesContainerProps) {
+	const searchParams = new URLSearchParams();
+	searchParams.append("filterName", filterName);
+	for (const key in filterDetail) {
+		const values = filterDetail[key as keyof FilterOptions];
+		if (values != null) {
+			searchParams.append(key, values.join(","));
+		}
+	}
 
-  const queryString = searchParams.toString();
+	const queryString = searchParams.toString();
 
-  const { data, error, isLoading } = useSWR<Employee[], Error>(
-    `/api/employees?${queryString}`,
-    employeesFetcher
-  );
-  useEffect(() => {
-    if (error != null) {
-      console.error(`Failed to fetch employees filtered by filterText`, error);
-    }
-  }, [error, filterText]);
-  if (data != null) {
-    return data.map((employee) => (
-      <EmployeeListItem employee={employee} key={employee.id} />
-    ));
-  }
-  if (isLoading) {
-    return <p>Loading employees...</p>;
-  }
+	const { data, error, isLoading } = useSWR<Employee[], Error>(
+		`/api/employees?${queryString}`,
+		employeesFetcher,
+	);
+	useEffect(() => {
+		if (error != null) {
+			console.error("Failed to fetch employees filtered by filterName", error);
+		}
+	}, [error]);
+	if (data != null) {
+		return data.map((employee) => (
+			<EmployeeListItem employee={employee} key={employee.id} />
+		));
+	}
+	if (isLoading) {
+		return <p>Loading employees...</p>;
+	}
 }
--- frontend/src/components/SearchEmployees.tsx
+++ frontend/src/components/SearchEmployees.tsx
@@ -1,134 +1,172 @@
 "use client";
-import { MenuItem, Paper, TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Container, Box, Typography, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import { type FilterOptions, employeeKeys } from "@/types/employee";
+import {
+	Box,
+	Button,
+	Checkbox,
+	Container,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	FormControlLabel,
+	FormGroup,
+	Paper,
+	TextField,
+	Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { EmployeeListContainer } from "./EmployeeListContainer";
-import { employeeKeys, FilterOptions } from "@/types/employee";
-
 
 export function SearchEmployees() {
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [searchDetail, setSearchDetail] = useState<FilterOptions | undefined>(undefined);
-  const [open, setOpen] = useState(false);
+	const [searchKeyword, setSearchKeyword] = useState("");
+	const [searchDetail, setSearchDetail] = useState<FilterOptions | undefined>(
+		undefined,
+	);
+	console.log("searchDetail", searchDetail);
 
+	const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
 
-  const handleClose = (data: FilterOptions) => {
-    setOpen(false);
-    setSearchDetail(data)
-  };
+	const handleAccept = (data: FilterOptions) => {
+		console.log("data", data);
 
-  return (
-    <Paper
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        flex: 1,
-        p: 2,
-      }}
-    >
-      <Container sx={{ display: "flex", flexDirection: "column",  alignItems: "center", justifyContent: "center"}}>
-        <TextField
-          placeholder="検索したい名前を入力してください"
-          value={searchKeyword}
-          onChange={(e) => setSearchKeyword(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <Button variant="text" onClick={handleClickOpen}>
-          詳細検索
-        </Button>
-      </Container>
-      <SearchModal handleClose={handleClose} open={open}/>
-      <EmployeeListContainer
-        key="employeesContainer"
-        filterText={searchKeyword}
-        filterDetail={searchDetail}
-      />
-    </Paper>
-  );
+		setOpen(false);
+		setSearchDetail(data);
+	};
+	const handleCancel = () => {
+		setOpen(false);
+	};
+
+	return (
+		<Paper
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				gap: 2,
+				flex: 1,
+				p: 2,
+			}}
+		>
+			<Container
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<TextField
+					placeholder="検索したい名前を入力してください"
+					value={searchKeyword}
+					onChange={(e) => setSearchKeyword(e.target.value)}
+					fullWidth
+					margin="normal"
+				/>
+				<Button variant="text" onClick={handleClickOpen}>
+					詳細検索
+				</Button>
+			</Container>
+			<SearchModal
+				handleAccept={handleAccept}
+				handleCancel={handleCancel}
+				open={open}
+			/>
+			<EmployeeListContainer
+				key="employeesContainer"
+				filterName={searchKeyword}
+				filterDetail={searchDetail}
+			/>
+		</Paper>
+	);
 }
 
 type SearchModalProps = {
-  handleClose: (data: FilterOptions) => void;
-  open: boolean;
-}
+	handleAccept: (data: FilterOptions) => void;
+	handleCancel: () => void;
+	open: boolean;
+};
 
+const SearchModal = ({
+	open,
+	handleAccept,
+	handleCancel,
+}: SearchModalProps) => {
+	// 仮データ
+	const [filterOptions, setFilterOptions] = useState<FilterOptions>({
+		department: ["Development", "Sales", "マーケティング"],
+		position: ["マネージャー", "デザイナー"],
+		skill: ["React", "Vue", "Go", "Photoshop", "Leadership"],
+	});
 
-const SearchModal = ({ open, handleClose }: SearchModalProps) => {
-  // 仮データ
-  const [filterOptions, setFilterOptions] = useState<FilterOptions>({
-    department: ["開発", "営業", "人事"],
-    position: ["リーダー", "メンバー"],
-    skill: ["React", "Vue", "Go"],
-  });
+	const [selectedFilters, setSelectedFilters] = useState<{
+		[key: string]: string[];
+	}>({});
 
-  const [selectedFilters, setSelectedFilters] = useState<{
-    [key: string]: string[];
-  }>({});
+	const handleCheck = (key: string, value: string) => {
+		setSelectedFilters((prev) => {
+			const current = prev[key] ?? [];
+			const newValues = current.includes(value)
+				? current.filter((v) => v !== value)
+				: [...current, value];
+			return {
+				...prev,
+				[key]: newValues,
+			};
+		});
+	};
 
-  console.log(selectedFilters);
-  
-
-  const handleCheck = (key: string, value: string) => {
-    setSelectedFilters((prev) => {
-      const current = prev[key] ?? [];
-      const newValues = current.includes(value)
-        ? current.filter((v) => v !== value)
-        : [...current, value];
-      return {
-        ...prev,
-        [key]: newValues,
-      };
-    });
-  };
-
-  return (
-    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
-      <DialogTitle>詳細検索</DialogTitle>
-      <DialogContent>
-        {Object.entries(employeeKeys).map(([key, label]) => (
-          <Box key={key} mb={2}>
-            <Typography variant="subtitle1" gutterBottom>
-              {label}
-            </Typography>
-            <FormGroup>
-              {filterOptions[key as keyof typeof employeeKeys]?.map((option) => (
-                <FormControlLabel
-                  key={option}
-                  control={
-                    <Checkbox
-                      checked={
-                        selectedFilters[key]?.includes(option) ?? false
-                      }
-                      onChange={() => handleCheck(key, option)}
-                    />
-                  }
-                  label={option}
-                />
-              ))}
-            </FormGroup>
-          </Box>
-        ))}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => {
-            handleClose(selectedFilters);
-          }}>
-          キャンセル
-        </Button>
-        <Button
-          onClick={() => {
-            handleClose(selectedFilters);
-          }}
-          variant="contained"
-        >
-          検索
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog
+			open={open}
+			onClose={() => handleAccept(selectedFilters)}
+			maxWidth="sm"
+			fullWidth
+		>
+			<DialogTitle>詳細検索</DialogTitle>
+			<DialogContent>
+				{Object.entries(employeeKeys).map(([key, label]) => (
+					<Box key={key} mb={2}>
+						<Typography variant="subtitle1" gutterBottom>
+							{label}
+						</Typography>
+						<FormGroup>
+							{filterOptions[key as keyof typeof employeeKeys]?.map(
+								(option) => (
+									<FormControlLabel
+										key={option}
+										control={
+											<Checkbox
+												checked={
+													selectedFilters[key]?.includes(option) ?? false
+												}
+												onChange={() => handleCheck(key, option)}
+											/>
+										}
+										label={option}
+									/>
+								),
+							)}
+						</FormGroup>
+					</Box>
+				))}
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={handleCancel} variant="contained">
+					キャンセル
+				</Button>
+				<Button
+					onClick={() => {
+						console.log("selectedFilters", selectedFilters);
+						handleAccept(selectedFilters);
+					}}
+				>
+					検索
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
 };
